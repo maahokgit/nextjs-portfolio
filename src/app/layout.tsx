e0559d9c7077dc975231b1b3da225133f28875cc
Edward Ma, MStJ <ma.edward@hotmail.com>
2025-01-03 23:09:59 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import Navigation from "@/components/Navigation/Navigation";
 import Footer from "@/components/Footer/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Edward Ma, MStJ - A Web Developer",
@@ -81,7 +70,7 @@ const RootLayout = (props: IRootLayoutProps) => {
           rel="stylesheet"
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Navigation />
         <main>{children}</main>
         <Footer />
